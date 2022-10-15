31c50f53c4a97edfbe914b96db9715136ed03c51
Ken choong <matchartproduction@gmail.com>
2022-10-16 03:07:58 +0800

    feat(cli): add and list offer entry point

--- src/api/cli/cli.spec.ts
+++ src/api/cli/cli.spec.ts
@@ -8,13 +8,20 @@ import {
 import { Cli } from './cli'
 import { container } from '../../container'
 import { TYPES } from '../../types'
+import { DeliveryTimeController } from '../../use-case/delivery-time/delivery-time.controller'
+import { DeliveryTimeService } from '../../use-case/delivery-time/delivery-time.service'
+import { OfferService } from '../../use-case/offer/offer.service'
 
 const mockedInquirerService = mock<InquireService>()
 const mockedDeliveryCostService = mock<DeliveryCostService>()
+const mockedDeliveryTimeController = mock<DeliveryTimeController>()
+const mockedOfferService = mock<OfferService>()
+
 describe('Cli', () => {
   let cli: Cli
   let deliveryCostService: DeliveryCostService
   let inquireService: InquireService
+  let offerService: OfferService
   beforeEach(() => {
     //
     container
@@ -24,11 +31,14 @@ describe('Cli', () => {
       .rebind(TYPES.DeliveryCostService)
       .toConstantValue(mockedDeliveryCostService)
 
+    container.rebind(TYPES.OfferService).toConstantValue(mockedOfferService)
+
     cli = container.get<Cli>(TYPES.Cli)
     deliveryCostService = container.get<DeliveryCostService>(
       TYPES.DeliveryCostService,
     )
     inquireService = container.get<InquireService>(TYPES.InquiryService)
+    offerService = container.get<OfferService>(TYPES.OfferService)
   })
 
   it('should be defined', () => {
@@ -45,5 +55,65 @@ describe('Cli', () => {
     await cli.start()
 
     expect(deliveryCostService.getDeliveryCost).toBeCalled()
+  })
+
+  it('should call getDeliveryTime', async () => {
+    mockedInquirerService.askTypeFunction.mockResolvedValue({
+      typeOfFunctionality: FunctionalityType.CalculateDeliveryTime,
+    })
+
+    await cli.start()
+
+    expect(deliveryTimeController.getPackageVechileDetails).toBeCalled()
+  })
+
+  it('should call create offer ', async () => {
+    mockedInquirerService.askTypeFunction.mockResolvedValue({
+      typeOfFunctionality: FunctionalityType.AddNewOffer,
+    })
+
+    mockedInquirerService.askCreateOfferQuestions.mockResolvedValue({
+      maxDistance: 10,
+      minDistance: 0,
+      maxWeight: 10,
+      minWeight: 0,
+      discountAmount: 5,
+      discountType: 'PERCENTAGE',
+      id: '1234',
+      offerCode: 'offercode1',
+    })
+
+    await cli.start()
+
+    expect(offerService.addOffers).toBeCalled()
+    expect(offerService.addOffers).toBeCalledWith({
+      criteria: {
+        distance: {
+          max: 10,
+          min: 0,
+          unitCode: 'km',
+        },
+        weight: {
+          max: 10,
+          min: 0,
+          unitCode: 'kg',
+        },
+      },
+      discountAmount: 5,
+      discountType: 'PERCENTAGE',
+      id: '1234',
+      offerCode: 'offercode1',
+      offerName: '5% off for courrier fees',
+    })
+  })
+
+  it('should call list offer ', async () => {
+    mockedInquirerService.askTypeFunction.mockResolvedValue({
+      typeOfFunctionality: FunctionalityType.ListOffers,
+    })
+
+    await cli.start()
+
+    expect(offerService.listOffers).toBeCalled()
   })
 })
--- src/api/cli/cli.ts
+++ src/api/cli/cli.ts
@@ -1,10 +1,13 @@
 import { inject, injectable } from 'inversify'
+import { DiscountType } from '../../domain/offer'
+import { CliTable } from '../../libs/cli-table'
 import { TYPES } from '../../types'
 import { DeliveryCostService } from '../../use-case/delivery-cost/delivery-cost.service'
 import {
   FunctionalityType,
   InquireService,
 } from '../../use-case/inquire/inquire.service'
+import { OfferService } from '../../use-case/offer/offer.service'
 
 export interface ICli {
   start(): void
@@ -17,6 +20,12 @@ export class Cli implements ICli {
     private inquiryService: InquireService,
     @inject(TYPES.DeliveryCostService)
     private deliveryCostService: DeliveryCostService,
+    @inject(TYPES.DeliveryTimeController)
+    private deliveryTimeController: DeliveryTimeController,
+    @inject(TYPES.OfferService)
+    private offerService: OfferService,
+    @inject(TYPES.CliTable)
+    private cliTable: CliTable,
   ) {}
 
   async start(): Promise<void> {
@@ -29,6 +38,49 @@ export class Cli implements ICli {
           await this.deliveryCostService.getDeliveryCost()
           break
 
+        case FunctionalityType.AddNewOffer:
+          const {
+            id,
+            offerCode,
+            discountAmount,
+            minWeight,
+            maxWeight,
+            minDistance,
+            maxDistance,
+          } = await this.inquiryService.askCreateOfferQuestions()
+
+          const creatOfferResult = await this.offerService.addOffers({
+            id,
+            offerCode,
+            offerName: `${discountAmount}% off for courrier fees`,
+            discountAmount,
+            discountType: DiscountType.PERCENTAGE,
+            criteria: {
+              weight: {
+                min: minWeight,
+                max: maxWeight,
+                unitCode: 'kg',
+              },
+              distance: {
+                min: minDistance,
+                max: maxDistance,
+                unitCode: 'km',
+              },
+            },
+          })
+
+          console.log(
+            'Offer created successfully ===> ',
+            JSON.stringify(creatOfferResult, null, 2),
+          )
+
+          break
+
+        case FunctionalityType.ListOffers:
+          const offers = await this.offerService.listOffers()
+
+          console.log('Found offer ==> ', JSON.stringify(offers, null, 2))
+          break
         default:
           break
       }
