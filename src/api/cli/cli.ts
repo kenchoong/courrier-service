eb0f97b900ef2453de49a843ff8409e93201a2ff
Ken choong <matchartproduction@gmail.com>
2022-10-16 00:40:55 +0800

    feat(cli): trigger delivery time controller function
    and test case

--- src/api/cli/cli.ts
+++ src/api/cli/cli.ts
@@ -1,6 +1,9 @@
 import { inject, injectable } from 'inversify'
+import { CliTable } from '../../libs/cli-table'
 import { TYPES } from '../../types'
 import { DeliveryCostService } from '../../use-case/delivery-cost/delivery-cost.service'
+import { DeliveryTimeController } from '../../use-case/delivery-time/delivery-time.controller'
+import { DeliveryTimeService } from '../../use-case/delivery-time/delivery-time.service'
 import {
   FunctionalityType,
   InquireService,
@@ -17,6 +20,10 @@ export class Cli implements ICli {
     private inquiryService: InquireService,
     @inject(TYPES.DeliveryCostService)
     private deliveryCostService: DeliveryCostService,
+    @inject(TYPES.DeliveryTimeController)
+    private deliveryTimeController: DeliveryTimeController,
+    @inject(TYPES.CliTable)
+    private cliTable: CliTable,
   ) {}
 
   async start(): Promise<void> {
@@ -27,6 +34,67 @@ export class Cli implements ICli {
       switch (typeOfFunctionality) {
         case FunctionalityType.CalculateDeliveryCost:
           await this.deliveryCostService.getDeliveryCost()
+          break
+        case FunctionalityType.CalculateDeliveryTime:
+          const cliTable = this.cliTable.initializeTable(
+            [
+              'Package Id',
+              'Discounted Amount',
+              'Total after discount',
+              'Estimated Delivery time',
+            ],
+            [20, 20, 20],
+            true,
+          )
+          const result =
+            await this.deliveryTimeController.getPackageVechileDetails()
+
+          /**
+             * [
+                {
+                  packageId: 'pkg1',
+                  totalDeliveryCostAfterDiscount: 750,
+                  totalDiscountedAmount: 0,
+                  estimatedDeliveryTime: 3.98
+                },
+                {
+                  packageId: 'pkg2',
+                  totalDeliveryCostAfterDiscount: 1475,
+                  totalDiscountedAmount: 0,
+                  estimatedDeliveryTime: 1.78
+                },
+                {
+                  packageId: 'pkg3',
+                  totalDeliveryCostAfterDiscount: 2350,
+                  totalDiscountedAmount: 0,
+                  estimatedDeliveryTime: 1.42
+                },
+                {
+                  packageId: 'pkg4',
+                  totalDeliveryCostAfterDiscount: 1395,
+                  totalDiscountedAmount: 105,
+                  estimatedDeliveryTime: 0.85
+                },
+                {
+                  packageId: 'pkg5',
+                  totalDeliveryCostAfterDiscount: 2125,
+                  totalDiscountedAmount: 0,
+                  estimatedDeliveryTime: 4.18
+                }
+              ]
+             */
+
+          result.map((item) => {
+            cliTable.push([
+              item.packageId,
+              item.totalDiscountedAmount,
+              item.totalDeliveryCostAfterDiscount,
+              item.estimatedDeliveryTime,
+            ])
+          })
+
+          console.log('Calculated delivery time for packages are as follows: ')
+          console.log(cliTable.toString())
           break
 
         default:
