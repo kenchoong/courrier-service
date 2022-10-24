c9474d4903170d46f15f760d0755ffa42b4d5c33
Ken choong <matchartproduction@gmail.com>
2022-10-24 21:01:09 +0800

    fix(delivery-time): getPackageComboPerTrip edge case algorithm
    and test case for all the possible edge case

--- src/use-case/delivery-time/utils/get-package-combo-per-trip.spec.ts
+++ src/use-case/delivery-time/utils/get-package-combo-per-trip.spec.ts
@@ -1,5 +1,8 @@
 import { PackageDtoForTime } from '../delivery-time.dto'
 import { getPackageComboPerTrip } from './get-package-combo-per-trip'
+import { mockFivePackagesShouldSendFirst } from './mocks/mock-five-packages-should-send-first.mock'
+import { mockedFourPackageShouldDeliveryFirst } from './mocks/mock-four-packages-should-send-first.mock'
+import { mockedThreePackagesShouldSendFirst } from './mocks/mock-three-packages-should-send-first..mock'
 
 describe('getPackageComboPerTrip', () => {
   let maxCarriableWeight = 200
@@ -189,5 +192,134 @@ describe('getPackageComboPerTrip', () => {
     )
 
     expect(result).toEqual(outcome)
+  })
+
+  it('getPackageComboPerTrip, Edge case, if sum of 3 packages still less than vechile max weight', () => {
+    const input = mockedThreePackagesShouldSendFirst
+
+    const mockedOutcome = [
+      {
+        packageId: 'pkg1',
+        weightInKg: 50,
+        distanceInKm: 100,
+        offerCode: 'off001',
+        baseDeliveryCost: 100,
+        sequence: 0,
+      },
+      {
+        packageId: 'pkg2',
+        weightInKg: 50,
+        distanceInKm: 100,
+        offerCode: 'off008',
+        baseDeliveryCost: 100,
+        sequence: 1,
+      },
+      {
+        packageId: 'pkg5',
+        weightInKg: 100,
+        distanceInKm: 100,
+        offerCode: 'off001',
+        baseDeliveryCost: 100,
+        sequence: 4,
+      },
+    ]
+
+    const result = getPackageComboPerTrip(input, maxCarriableWeight)
+
+    expect(result).toEqual(mockedOutcome)
+  })
+
+  it('getPackageComboPerTrip, Edge case, if sum of 4 packages still less than vechile max weight', () => {
+    const input = mockedFourPackageShouldDeliveryFirst
+
+    const mockedOutcome = [
+      {
+        packageId: 'pkg1',
+        weightInKg: 30,
+        distanceInKm: 100,
+        offerCode: 'off002',
+        baseDeliveryCost: 100,
+        sequence: 0,
+      },
+      {
+        packageId: 'pkg2',
+        weightInKg: 50,
+        distanceInKm: 100,
+        offerCode: 'off001',
+        baseDeliveryCost: 100,
+        sequence: 1,
+      },
+      {
+        packageId: 'pkg3',
+        weightInKg: 40,
+        distanceInKm: 100,
+        offerCode: 'off003',
+        baseDeliveryCost: 100,
+        sequence: 2,
+      },
+      {
+        packageId: 'pkg4',
+        weightInKg: 70,
+        distanceInKm: 100,
+        offerCode: 'off001',
+        baseDeliveryCost: 100,
+        sequence: 3,
+      },
+    ]
+
+    const result = getPackageComboPerTrip(input, maxCarriableWeight)
+
+    expect(result).toEqual(mockedOutcome)
+  })
+
+  it('getPackageComboPerTrip, Edge case, if sum of 5 packages still less than vechile max weight', () => {
+    const input = mockFivePackagesShouldSendFirst
+
+    const mockedOutcome = [
+      {
+        packageId: 'pkg1',
+        weightInKg: 30,
+        distanceInKm: 100,
+        offerCode: 'off002',
+        baseDeliveryCost: 100,
+        sequence: 0,
+      },
+      {
+        packageId: 'pkg2',
+        weightInKg: 50,
+        distanceInKm: 100,
+        offerCode: 'off001',
+        baseDeliveryCost: 100,
+        sequence: 1,
+      },
+      {
+        packageId: 'pkg3',
+        weightInKg: 40,
+        distanceInKm: 100,
+        offerCode: 'off003',
+        baseDeliveryCost: 100,
+        sequence: 2,
+      },
+      {
+        packageId: 'pkg4',
+        weightInKg: 70,
+        distanceInKm: 100,
+        offerCode: 'off001',
+        baseDeliveryCost: 100,
+        sequence: 3,
+      },
+      {
+        packageId: 'pkg5',
+        weightInKg: 5,
+        distanceInKm: 100,
+        offerCode: 'off001',
+        baseDeliveryCost: 100,
+        sequence: 4,
+      },
+    ]
+
+    const result = getPackageComboPerTrip(input, maxCarriableWeight)
+
+    expect(result).toEqual(mockedOutcome)
   })
 })
--- src/use-case/delivery-time/utils/get-package-combo-per-trip.ts
+++ src/use-case/delivery-time/utils/get-package-combo-per-trip.ts
@@ -1,112 +1,53 @@
 import { PackageDtoForTime } from '../delivery-time.dto'
+import { decideSendFirstCombo } from './decide-send-first-combo'
 
-/**
- *
- * Given list of package, with their distance and weight
- * Given the max load of all our vechiles
- *
- * Calculate and select the combination of package weight
- * that is closest to the max load of our vechiles
- * for 1 trip
- *
- * Given:
- * maxLoad = 200
- * packageList(only weight)= [ 50, 75 , 175, 110, 155 ]
- *
- * get the highest combination of package weight that is closest to maxLoad
- * Output: [ 75, 110 ]
- * Then return the list 2nd and 4th package from the packageList
- *
- * if no combination of package weight is found,
- * return highest weight package into the list
- *
- * @param packageList (List of packages to be delivered)
- * @param maxCarriableWeight
- * @returns packageList[] (List of packages to be delivered in 1 trip)
- */
+export function powerSet(array: PackageDtoForTime[]): PackageDtoForTime[][] {
+  if (array.length === 0) {
+    return [[]]
+  }
+
+  var lastElement = array.pop() as PackageDtoForTime
+
+  var restPowerset = powerSet(array)
+
+  var powerset = []
+  for (var i = 0; i < restPowerset.length; i++) {
+    var set = restPowerset[i]
+
+    // without last element
+    powerset.push(set)
+
+    // with last element
+    set = set.slice() // create a new array that's a copy of set
+    set.push(lastElement)
+    powerset.push(set)
+  }
+
+  return powerset
+}
+
 export function getPackageComboPerTrip(
   packageList: PackageDtoForTime[],
   maxCarriableWeight: number,
 ) {
-  let possiblePackagesCombo: PackageDtoForTime[][] = []
+  // all subsets of arr
+  var powerset = powerSet(packageList)
 
-  const originalState = { weightInKg: 0 } as PackageDtoForTime
+  // subsets summing less than or equal to number
+  var subsets: PackageDtoForTime[][] = []
 
-  // Given package weight = [ 50, 75 , 175, 110, 155 ]
-  // Generate all possible combination of package
-  // Will produce result like this:
-  // [
-  //   [ 50 ],      [ 75 ],
-  //   [ 50, 75 ],  [ 175 ],
-  //   [ 110 ],     [ 50, 110 ],
-  //   [ 75, 110 ], [ 155 ]
-  // ]
-  // For M << 1, left shift in js, reference below:
-  // https://stackoverflow.com/questions/38922606/what-is-x-1-and-x-1
-  // https://stackoverflow.com/a/49143693/4332049
-  let M = 1 << packageList.length
-  for (var i = 1; i < M; ++i) {
-    var sublist = packageList.filter(function (c, k) {
-      return (i >> k) & 1
-    })
-    if (
-      sublist.reduce((previous, current) => {
-        return {
-          ...current,
-          weightInKg: previous.weightInKg + current.weightInKg,
-        }
-      }, originalState).weightInKg <= maxCarriableWeight
-    ) {
-      possiblePackagesCombo.push(sublist)
+  for (var i = 0; i < powerset.length; i++) {
+    var subset = powerset[i]
+
+    var sum = 0
+    for (var j = 0; j < subset.length; j++) {
+      sum += subset[j].weightInKg
+    }
+
+    if (sum <= maxCarriableWeight) {
+      subsets.push(subset)
     }
   }
 
-  // sum up each element inside each element(array) inside possiblePackagesCombo array
-  // sum only the weightInKg property
-  // from possible package above
-  // produce this stimulation below.
-  // sumUpAlpossiblePackagesComboInList = [
-  //     50,  75, 125, 175,
-  //    110, 160, 185, 155
-  //  ]
-  // (stimution value above only for weightInKg property)
-  const sumUpAlpossiblePackagesComboInList: PackageDtoForTime[] =
-    possiblePackagesCombo.map((x) =>
-      x.reduce(
-        (previous, current) => ({
-          ...current,
-          weightInKg: previous.weightInKg + current.weightInKg,
-        }),
-        originalState,
-      ),
-    )
-
-  // Find the highest combination of package weight that is closest to maxLoad
-  // from sumUpAlpossiblePackagesComboInList
-  // produce 185 (closest to maxCarriableWeight = 200)
-  // therefore get the whole object
-  const calculateClosetCombination: PackageDtoForTime =
-    sumUpAlpossiblePackagesComboInList.reduce((prev, current) => {
-      const prevWeight = Math.abs(prev.weightInKg - maxCarriableWeight)
-
-      const currentWeight = Math.abs(current.weightInKg - maxCarriableWeight)
-      if (currentWeight < prevWeight) {
-        return current
-      } else {
-        return prev
-      }
-    }, originalState)
-
-  // From sumUpAlpossiblePackagesComboInList
-  // get index of calculated closest combination
-  const indexOfItem = sumUpAlpossiblePackagesComboInList.indexOf(
-    calculateClosetCombination,
-  )
-
-  // possiblePackagesCombo[indexOfItem]  = sumUpAlpossiblePackagesComboInList[indexOfItem]
-  // This is the combination of package weight that is closest to maxLoad
-  // Which is [ 75, 110 ]
-  const closestCombination = possiblePackagesCombo[indexOfItem]
-
-  return closestCombination
+  return decideSendFirstCombo(subsets)
 }
